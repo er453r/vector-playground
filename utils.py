221f3b168400fa13e0ca11f6494fa1c99d13f5ee
Marcin Kotz <marcin.kotz@ives-system.com>
2019-12-13 15:22:20 +0100

    center finding works

--- utils.py
+++ utils.py
@@ -1,5 +1,11 @@
 import cv2
 import cv2.aruco as aruco
+import numpy as np
+import math
+
+
+def average_points(points):
+    return
 
 
 def debug_image(image):
@@ -8,5 +14,10 @@ def debug_image(image):
     parameters = aruco.DetectorParameters_create()
     corners, ids, rejected_img_points = aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
     frame_markers = aruco.drawDetectedMarkers(image.copy(), corners, ids)
+
+    corners = np.asarray(corners)
+    points = corners.reshape((math.floor(np.prod(corners.shape) / 2), 2))
+    center = np.divide(np.sum(points, axis=0), points.shape[0])
+
 
     cv2.imshow("debug", frame_markers)
