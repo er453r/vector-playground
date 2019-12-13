49adca2c8bef10897c88e41bceb5b827fdbc79f5
Marcin Kotz <marcin.kotz@ives-system.com>
2019-12-13 15:45:22 +0100

    closest corner finding

--- utils.py
+++ utils.py
@@ -16,8 +16,16 @@ def debug_image(image):
     frame_markers = aruco.drawDetectedMarkers(image.copy(), corners, ids)
 
     corners = np.asarray(corners)
+    sort_indicies = np.argsort(ids, axis=0)
+    corners = corners[sort_indicies]
     points = corners.reshape((math.floor(np.prod(corners.shape) / 2), 2))
     center = np.divide(np.sum(points, axis=0), points.shape[0])
 
+    corner = corners[0]
+    corner_points = corner.reshape((math.floor(np.prod(corner.shape) / 2), 2))
+
+    distances = [np.linalg.norm(point - center) for point in corner_points]
+    min_index = np.argmin(distances)
+    closest_corner = corner_points[min_index]
 
     cv2.imshow("debug", frame_markers)
