b0e6f23fe59f113ad38e9c2c45079a96a0898757
Marcin Kotz <marcin.kotz@ives-system.com>
2019-12-19 13:40:07 +0100

    marker detection works

--- utils.py
+++ utils.py
@@ -13,7 +13,7 @@ def show(image, title, scale=0.5):
     cv2.imshow(title, resized)
 
 
-def perspective_transform(img, corners):
+def perspective_transform(img, corners, margin=0):
     height = max(np.linalg.norm(corners[0] - corners[3]),
                  np.linalg.norm(corners[1] - corners[2]))
     width = max(np.linalg.norm(corners[0] - corners[1]),
@@ -24,8 +24,6 @@ def perspective_transform(img, corners):
     ratio_diff = abs(target_ratio - ratio)
 
     print(f'ratio is {ratio} diff {ratio_diff}, {width}x{height}')
-
-    margin = 32
 
     layout = np.array([[margin, margin], [width - margin, margin], [width - margin, height - margin], [margin, height - margin]], np.float32)
 
@@ -65,6 +63,23 @@ def outer_corners(corners):
     return np.asarray(farthest_corners)
 
 
+def average_coords(image, width, height, x, y):
+    error = 0.8
+    margin = (1 - error) / 2
+
+    box_width = int(image.shape[1]) / width
+    box_height = int(image.shape[0]) / height
+
+    x1 = (x + 0 + margin) * box_width
+    x2 = (x + 1 - margin) * box_width
+    y1 = (y + 0 + margin) * box_height
+    y2 = (y + 1 - margin) * box_height
+
+    roi = image[int(y1):int(y2), int(x1):int(x2)]
+
+    return np.linalg.norm(cv2.mean(roi))
+
+
 def debug_image(image):
     corners, ids, frame_markers = markers(image)
 
@@ -73,18 +88,48 @@ def debug_image(image):
     for i in farthest_corners:
         cv2.circle(frame_markers, tuple(i), 30, 255, -1)
 
-    corners, ids, cropped = markers(perspective_transform(image, farthest_corners))
-
+    transformed = perspective_transform(image, farthest_corners, margin=32)
+    corners, ids, cropped = markers(transformed)
     marker_size = np.linalg.norm(corners[0][0][0][0] - corners[0][0][0][1])
-
     farthest_corners = outer_corners(corners)
-    image_width = np.linalg.norm(farthest_corners[0] - farthest_corners[1])
-    image_height = np.linalg.norm(farthest_corners[1] - farthest_corners[2])
 
-    width = round(image_width / marker_size)
-    height = round(image_height / marker_size)
+    final = perspective_transform(transformed, farthest_corners)
+    final_image = cv2.cvtColor(final, cv2.COLOR_BGR2GRAY)
+    width = int(final_image.shape[1] / marker_size)
+    height = int(final_image.shape[0] / marker_size)
 
-    print(f"Table size {width}x{height} - marker size {marker_size}, image {image_width}x{image_height}")
+    field_width = final_image.shape[1] / width
+    field_height = final_image.shape[0] / height
 
-    show(cropped, "cropped", 0.5)
+    print(f"Table size {width}x{height} - marker size {marker_size}, image {final_image.shape[1]}x{final_image.shape[0]}")
+
+    average_blanks = [average_coords(final_image, width, height, x, y) for y in range(1, height-1) for x in [0, width-1]]
+    average_blank = sum(average_blanks) / len(average_blanks)
+
+    print(f"average blank {average_blank}")
+
+    blank_margin = 10
+
+    marked = []
+
+    for y in range(1, height):
+        for x in range(1, width-1):
+            average = average_coords(final_image, width, height, x, y)
+
+            diff = average_blank - average
+
+            print(f"field {x}x{y} diff {diff}")
+
+            if diff > blank_margin:
+                marked.append((x, y))
+
+    print(f"Marked size {len(marked)}")
+
+    for point in marked:
+        x, y = point
+        position = (int((x + 0.5) * field_width), int((y + 0.5) * field_height))
+
+        cv2.circle(final, position, 30, (0, 255, 0), -1)
+
+    show(final, "cropped", 0.5)
     show(frame_markers, "debug", 0.5)
